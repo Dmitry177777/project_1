e0ce411ba11368bb4f1aced73e3276dad1f7f009
Dmitry <k1791@mail.ru>
2023-01-25 23:12:11 +0500

    add func: upper_string (rename) capital_string (add+docstring)

--- str_func.py
+++ str_func.py
@@ -1,6 +1,12 @@
-def capital_string (str_i):
-""" Возврат строки с заглавными буквами """ 
-str_result = str (str_i)
-  return str_result.upper()
+def upper_string (str_i):
+ """ Возврат строки с заглавными буквами """
+ str_result = str (str_i)
+ return str_result.upper()
 
+def capital_string (str_i):
+ """ Возврат строки с первыми заглавными буквами """
+ str_result = str(str_i)
+ return str_result.capitalize()
+
+#print (upper_string ("снег"))
 #print (capital_string ("снег"))
